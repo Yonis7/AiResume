f5ebf045589e71849c04daab3d9f68f72e0a3c36
Yonis7 <yonisaden7@gmail.com>
2025-08-21 20:06:30 -0400

    Ui almost complete. TODO: fileuploader backend and upload resume

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,10 +1,11 @@
-import {useState, type FormEvent} from 'react'
-import Navbar from "../components/Navbar";
-import FileUploader from "../components/FileUploader";
-import { usePuterStore } from '~/lib/puter';
-import { useNavigate } from 'react-router';
-
-
+import {type FormEvent, useState} from 'react'
+import Navbar from "~/components/Navbar";
+import FileUploader from "~/components/FileUploader";
+import {usePuterStore} from "~/lib/puter";
+import {useNavigate} from "react-router";
+import {convertPdfToImage} from "~/lib/pdf2img";
+import {generateUUID} from "~/lib/utils";
+import {prepareInstructions} from "../../constants";
 const Upload = () => {
     const { auth, isLoading, fs, ai, kv } = usePuterStore();
     const navigate = useNavigate();
